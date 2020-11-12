e16b6545201a70d63afc1030cc77efd61060a880
James D Vieyra <james@vieyra.co.za>
2020-11-13 00:04:14 +0200

    Settup Factory Method & got 1 miximo npc ready

---
+++ Assets/Scripts/NPC_Factory/NPC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Types
+{
+    npc1,
+    npc2
+}
+public abstract class NPC : MonoBehaviour
+{    
+    public abstract string Name { get; set; }
+    public abstract GameObject gameObject { get; set; }
+
+    public string RandomDialogue(string [] dialogueChoices)
+    {
+        string output = "";
+
+        return output;
+    }
+
+    public abstract void WalkTo();
+
+    public abstract void TalkTO(string type); 
+        
+}
---
+++ Assets/Scripts/NPC_Factory/NPC1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC1 : NPC
+{    
+    public override string Name { get; set; }
+    public override GameObject gameObject { get; set; }
+
+    public void Start()
+    {
+        gameObject = GameObject.FindGameObjectWithTag(Types.npc1.ToString());
+    }
+
+    public override void TalkTO(string type)
+    {    
+        throw new System.NotImplementedException();
+    }
+
+    public override void WalkTo()
+    {
+        
+        throw new System.NotImplementedException();
+        
+    }
+    
+}
