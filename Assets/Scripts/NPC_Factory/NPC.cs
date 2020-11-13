2327f7e59a37210f2b8d26c98eb05dee7d04469f
James D Vieyra <james@vieyra.co.za>
2020-11-13 14:59:35 +0200

    eotk

--- Assets/Scripts/NPC_Factory/NPC.cs
+++ Assets/Scripts/NPC_Factory/NPC.cs
@@ -9,8 +9,12 @@ public enum Types
 }
 public abstract class NPC : MonoBehaviour
 {    
-    public abstract string Name { get; set; }
-    public abstract GameObject gameObject { get; set; }
+    public new abstract string name { get; set; }
+    public new abstract GameObject gameObject { get; set; }
+    public abstract GameObject pos1 { get; set; }
+    public abstract GameObject pos2 { get; set; }
+    public abstract bool goBack { get; set; }
+    public abstract string[] fluffTextConversations { get; set; }
 
     public string RandomDialogue(string [] dialogueChoices)
     {
@@ -19,7 +23,20 @@ public abstract class NPC : MonoBehaviour
         return output;
     }
 
-    public abstract void WalkTo();
+    public abstract void SetDestination(Transform _destination);
+
+    public void Patrol()
+    {
+        if(goBack == false)
+        {
+            SetDestination(pos1.transform);
+           
+        }        
+        else if(goBack == true)
+        {
+            SetDestination(pos2.transform);            
+        }
+    }
 
     public abstract void TalkTO(string type); 
         
--- Assets/Scripts/NPC_Factory/NPC1.cs
+++ Assets/Scripts/NPC_Factory/NPC1.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPC1 : NPC
 {    
-    public override string Name { get; set; }
+    public override string name { get; set; }
     public override GameObject gameObject { get; set; }
+    public override string[] fluffTextConversations { get; set; }
+    public override bool goBack { get; set; }    
+    public override GameObject pos1 { get { return Pos1; } set {  Pos1 = value; } }    
+    public override GameObject pos2 { get { return Pos2; } set { Pos2 = value; } }
+    [SerializeField]
+    GameObject Pos1;
+    [SerializeField]
+    GameObject Pos2;
+
+    NavMeshAgent navMeshAgent;
+
 
     public void Start()
     {
+        #region
         gameObject = GameObject.FindGameObjectWithTag(Types.npc1.ToString());
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+        goBack = false;
+        #endregion        
     }
 
     public override void TalkTO(string type)
@@ -17,11 +33,34 @@ public class NPC1 : NPC
         throw new System.NotImplementedException();
     }
 
-    public override void WalkTo()
+    public override void SetDestination(Transform _destination)
     {
-        
-        throw new System.NotImplementedException();
-        
+        if(_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            navMeshAgent.SetDestination(targetVector);
+            //if (navMeshAgent.pathStatus == NavMeshPathStatus.PathComplete)
+            //{
+            //    SetDestination(pos2.transform);
+            //    goBack = !goBack;
+            //}
+                
+            
+
+        }
     }
-    
+        
+    private void Update()
+    {
+        try
+        {
+            Patrol();
+            if (gameObject.transform.position == pos1.transform.position)
+                Debug.Log("hi");
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError(ex);
+        }
+    }
 }
